9f2f3237d414dfa0d73d0e47f8e0e6fa36064bac
Simone <info@simoneicardi.com>
2025-10-15 12:05:50 +0200

    aggiugi rotta menu pizze

--- app.js
+++ app.js
@@ -5,11 +5,43 @@ const app = express();
 // impostiano un ref per il numero della porta
 const port = 3000;
 
-// app.use(express.static('public'));
+// usiamo il middleware static di express (per rendere disponibile i file statici)
+app.use(express.static('public'));
 
 // impostiamo la rotta di index
 app.get("/", (req, res) => {
-    // codice funzione
+    res.send('<h1>Ecco la home della API della nostra pizzeria</h1>')
+})
+
+// impostiamo la rotta di menu
+app.get("/menu", (req, res) => {
+    // creiamo l'array di oggetti da ritornare sulla rotta
+    const menu = [
+        {
+            name: "Margherita",
+            image: "imgs/pizze/margherita.webp",
+            ingredients: ["pomodoro", "mozzarella"],
+        }, {
+            name: "Marinara",
+            image: "imgs/pizze/marinara.jpeg",
+            ingredients: ["pomodoro", "aglio", "origano"],
+        }, {
+            name: "Diavola",
+            image: "imgs/pizze/diavola.jpeg",
+            ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+        }, {
+            name: "Bufalina",
+            image: "imgs/pizze/bufalina.jpeg",
+            ingredients: ["pomodoro", "mozzarella di bufala"],
+        }, {
+            name: "4 formaggi",
+            image: "imgs/pizze/4_formaggi.jpeg",
+            ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
+        }
+    ];
+
+    // inviamo la risposta con il json relativo
+    res.json(menu)
 })
 
 // mettiamo in ascolto il server sulla porta definita
